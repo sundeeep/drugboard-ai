eef2a8b4af49966a3dda5ddf2b69a12b74b755af
sandeepkdasari143 <sandeepkumar25820@gmail.com>
2024-01-16 19:53:31 +0530

    patchwork04

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        port: "",
-        pathname: "/dj4yzosa6/image/upload/v1691598075/drugboard.ai/**",
-      },
-    ],
-  },
-};
+const nextConfig = {}
 
 module.exports = nextConfig
