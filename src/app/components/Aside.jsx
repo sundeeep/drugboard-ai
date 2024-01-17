796894e5c9ea562ff2ce383b471092cf78b65e19
sandeepkdasari143 <sandeepkumar25820@gmail.com>
2024-01-17 14:19:08 +0530

    Deployment Test

--- src/app/components/Aside.jsx
+++ src/app/components/Aside.jsx
@@ -1,29 +1,28 @@
-
-import Image from "next/image";
+import { CldImage } from "next-cloudinary";
 
 const Aside = () => {
     
     return (
         <aside className="flex flex-col justify-between items-start">
-            {/* Logo */} 
+            {/* Logo */}
             <div className="">
-                <Image
-                    src="/drugboardLogo.png"
-                    alt="Drugboard.ai Logo"
-                    width="250"
-                    height="100"
-                />
+            <img
+                src="https://res.cloudinary.com/dj4yzosa6/image/upload/v1691598075/drugboard.ai/logo_new_final_with_tagline_dccivd.png"
+                alt="Drugboard.ai Logo"
+                width="250"
+                height="100"
+            />
             </div>
 
             {/* Navbar */}
             <nav>
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
             </nav>
             {/* Logout Button */}
         </aside>
