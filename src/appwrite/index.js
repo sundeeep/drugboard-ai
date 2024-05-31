843887c50bb533a098264a528568a829e7499d13
sandeepkdasari143 <sandeepkumar25820@gmail.com>
2024-05-31 13:59:36 +0530

    random

--- src/appwrite/index.js
+++ src/appwrite/index.js
@@ -3,6 +3,6 @@ import { Client } from "appwrite";
 const appwriteClient = new Client();
 
 appwriteClient
-  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setEndpoint("https://appwrite369.drugboard.ai/v1")
   .setProject("65a22a9e2eaa88f35d94");
 export { appwriteClient };
